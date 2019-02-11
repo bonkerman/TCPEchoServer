993059bd8478407da807ae53d946eab1bf503959
bonkerman <35398582+bonkerman@users.noreply.github.com>
2019-02-11 15:22:08 +0200

    Using Sockets

--- Program.cs
+++ Program.cs
@@ -1,64 +1,143 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace TCP
-{
-    class Program
-    {
-        private static CancellationTokenSource cancellation;
-        private static int totalTaskNr = 0;
-        private static async Task Main(string[] args)
-        {
-        var listener = new TcpListener(IPAddress.IPv6Any, 10000);  
-        
-        listener.Start();  
-        try
-        {   
-           cancellation = new CancellationTokenSource();
-           await Task.WhenAll(
-                Enumerable.Range(0, Environment.ProcessorCount)
-                    .Select( _ => BeginConversation(listener.AcceptTcpClientAsync()))
-            );
-            
-        }
-       finally
-       {
-           listener.Stop();
-           System.Console.WriteLine("Done");
-       }
-    }
-
-        private static async Task BeginConversation(Task<TcpClient> clientTask){
-            var tasknr=++totalTaskNr;
-            var client = await clientTask;
-            Console.WriteLine("{0} Begun with " + client.Client.RemoteEndPoint.ToString(), tasknr);
-            using(var ns = client.GetStream()){
-                try
-                {
-                    while(!cancellation.Token.IsCancellationRequested){
-                        var buffer = new byte[client.ReceiveBufferSize];
-                        var size =await Task.Run(()=>ns.ReadAsync(buffer, 0, client.ReceiveBufferSize), cancellation.Token);
-                            if(size==0){
-                                System.Console.WriteLine(tasknr+") recieved data is null; Exiting;");
-                                break;
-                            }
-                        await Task.Run(()=>ns.WriteAsync(buffer, 0, size), cancellation.Token);
-                    }
-                }
-                finally
-                {
-                    System.Console.WriteLine(tasknr+" done");
-                    client.GetStream().Close();
-                    client.Close();
-                }
-                
-            }
-            
-        }
-    }
+using System;  
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;  
+using System.Threading;  
+  
+// State object for reading client data asynchronously  
+public class StateObject {  
+    public Socket workSocket = null;  
+    public const int BufferSize = 1024;  
+    public byte[] buffer = new byte[BufferSize];  
+}  
+  
+public class AsynchronousSocketListener {  
+    public static ManualResetEvent allDone = new ManualResetEvent(false);  
+    public static void StartListening() {    
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.IPv6Loopback, 10000);  
+        Socket listener = new Socket(IPAddress.IPv6Loopback.AddressFamily,  
+            SocketType.Stream, ProtocolType.Tcp );  
+        try {  
+            listener.Bind(localEndPoint);  
+            listener.Listen(100);  
+            while (true) {   
+                allDone.Reset();    
+                Console.WriteLine("Waiting for a connection...");  
+                listener.BeginAccept(   
+                    new AsyncCallback(AcceptCallback),  
+                    listener );  
+                allDone.WaitOne();  
+            }  
+        } catch (Exception e) {  
+            Console.WriteLine(e.ToString());  
+        }  
+        Console.WriteLine("\nPress ENTER to continue...");  
+        Console.Read();  
+    }  
+  
+    public static void AcceptCallback(IAsyncResult ar) {  
+        allDone.Set();   
+        Socket listener = (Socket) ar.AsyncState;  
+        Socket handler = listener.EndAccept(ar);  
+        System.Console.WriteLine("Connection recieved from "+ handler.RemoteEndPoint);  
+        StateObject state = new StateObject();  
+        state.workSocket = handler;  
+        handler.BeginReceive( state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);  
+    }  
+  
+    public static void ReadCallback(IAsyncResult ar) {  
+        StateObject state = (StateObject) ar.AsyncState; 
+        int bytesRead = state.workSocket.EndReceive(ar);
+        Send(state, state.buffer);  
+    } 
+  
+    private static void Send(StateObject state, byte[] data) {  
+        state.workSocket.BeginSend(state.buffer, 0, data.Length, 0, new AsyncCallback(SendCallback), state);  
+    }  
+  
+    private static void SendCallback(IAsyncResult ar) {  
+        try {  
+            var state = (StateObject) ar.AsyncState;
+            int bytesSent = state.workSocket.EndSend(ar); 
+            state.workSocket.BeginReceive( state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state); 
+        } catch (Exception e) {  
+            Console.WriteLine(e.ToString());  
+        }  
+    }  
+  
+    public static int Main(String[] args) {  
+        StartListening();  
+        return 0;  
+    }  
+}using System;  
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;  
+using System.Threading;  
+  
+// State object for reading client data asynchronously  
+public class StateObject {  
+    public Socket workSocket = null;  
+    public const int BufferSize = 1024;  
+    public byte[] buffer = new byte[BufferSize];  
+}  
+  
+public class AsynchronousSocketListener {  
+    public static ManualResetEvent allDone = new ManualResetEvent(false);  
+    public static void StartListening() {    
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.IPv6Loopback, 10000);  
+        Socket listener = new Socket(IPAddress.IPv6Loopback.AddressFamily,  
+            SocketType.Stream, ProtocolType.Tcp );  
+        try {  
+            listener.Bind(localEndPoint);  
+            listener.Listen(100);  
+            while (true) {   
+                allDone.Reset();    
+                Console.WriteLine("Waiting for a connection...");  
+                listener.BeginAccept(   
+                    new AsyncCallback(AcceptCallback),  
+                    listener );  
+                allDone.WaitOne();  
+            }  
+        } catch (Exception e) {  
+            Console.WriteLine(e.ToString());  
+        }  
+        Console.WriteLine("\nPress ENTER to continue...");  
+        Console.Read();  
+    }  
+  
+    public static void AcceptCallback(IAsyncResult ar) {  
+        allDone.Set();   
+        Socket listener = (Socket) ar.AsyncState;  
+        Socket handler = listener.EndAccept(ar);  
+        System.Console.WriteLine("Connection recieved from "+ handler.RemoteEndPoint);  
+        StateObject state = new StateObject();  
+        state.workSocket = handler;  
+        handler.BeginReceive( state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);  
+    }  
+  
+    public static void ReadCallback(IAsyncResult ar) {  
+        StateObject state = (StateObject) ar.AsyncState; 
+        int bytesRead = state.workSocket.EndReceive(ar);
+        Send(state, state.buffer);  
+    } 
+  
+    private static void Send(StateObject state, byte[] data) {  
+        state.workSocket.BeginSend(state.buffer, 0, data.Length, 0, new AsyncCallback(SendCallback), state);  
+    }  
+  
+    private static void SendCallback(IAsyncResult ar) {  
+        try {  
+            var state = (StateObject) ar.AsyncState;
+            int bytesSent = state.workSocket.EndSend(ar); 
+            state.workSocket.BeginReceive( state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state); 
+        } catch (Exception e) {  
+            Console.WriteLine(e.ToString());  
+        }  
+    }  
+  
+    public static int Main(String[] args) {  
+        StartListening();  
+        return 0;  
+    }  
 }
